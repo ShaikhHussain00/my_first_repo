6f98f1de9c2e37e1fb378ba453141c55ef2fbd06
ShaikhHussain00 <shaikhhussainusman0010@gmail.com>
2023-04-02 14:59:28 +0530

    modified mod

--- sum.py
+++ sum.py
@@ -10,12 +10,17 @@ def mul(a,b):
 def div(a,b):
     return a/b
 
+def mod(a,b):
+    return a%b
+
 c = sum(10, 20)
 d = diff(10,20)
 p = mul(10,20)
 q = div(20,10)
+h = mod(10,20)
 
 print(c)
 print(d)
 print(q)
 print(p)
+print(h)
